e5978bbd718f2f05f89e1184604a7e9a260f126b
xNovo3000 <fontana.ciro.2001@gmail.com>
2021-10-07 18:45:03 +0200

    Ended new realtime view

--- lib/view/realtime_smartphone_2.dart
+++ lib/view/realtime_smartphone_2.dart
@@ -1,12 +1,19 @@
 import 'package:elis_analytics_dashboard/component/colored_app_bar.dart';
 import 'package:elis_analytics_dashboard/component/modal/fullscreen/error.dart';
 import 'package:elis_analytics_dashboard/component/modal/fullscreen/wait.dart';
+import 'package:elis_analytics_dashboard/model/container/vodafone_daily.dart';
+import 'package:elis_analytics_dashboard/model/container/vodafone_daily_list.dart';
 import 'package:elis_analytics_dashboard/model/data/sensor.dart';
+import 'package:elis_analytics_dashboard/model/data/vodafone_cluster.dart';
 import 'package:elis_analytics_dashboard/model/data/weather_instant.dart';
+import 'package:elis_analytics_dashboard/model/enum/kpi.dart';
+import 'package:elis_analytics_dashboard/model/enum/nationality.dart';
 import 'package:elis_analytics_dashboard/model/enum/room.dart';
 import 'package:elis_analytics_dashboard/model/inherited/error.dart';
 import 'package:elis_analytics_dashboard/model/inherited/realtime_data.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:weather_icons/weather_icons.dart';
 
 class ViewRealtimeSmartphone extends StatelessWidget {
@@ -62,6 +69,15 @@ class _ViewRealtimeSmartphoneData extends StatelessWidget {
         SliverToBoxAdapter(
           child: _ComponentWeatherReport(
             weatherInstant: data.weather,
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Divider(indent: 8, endIndent: 8),
+        ),
+        SliverToBoxAdapter(
+          child: _ComponentPredictions(
+            campusVodafoneData: data.campusVodafoneData,
+            neighborhoodVodafoneData: data.neighborhoodVodafoneData,
           ),
         ),
       ],
@@ -268,10 +284,10 @@ class _ComponentWeatherReport extends StatelessWidget {
         ),
         ListTile(
           leading: BoxedIcon(weatherInstant.icon, color: weatherInstant.iconColor),
-          title: Text('Temperatura: ${weatherInstant.ambientTemperature.floor()}°C'),
+          title: Text('Temperatura: ${weatherInstant.ambientTemperature.round()}°C'),
           subtitle: Text(
-            'Vento: ${weatherInstant.windSpeed.floor()} km/h ${weatherInstant.windDirection} · '
-            'Umidità: ${weatherInstant.humidity.floor()}%'
+            'Vento: ${weatherInstant.windSpeed.round()} km/h ${weatherInstant.windDirection} · '
+            'Umidità: ${weatherInstant.humidity.round()}%'
           ),
           // isThreeLine: true,
         ),
@@ -281,3 +297,103 @@ class _ComponentWeatherReport extends StatelessWidget {
 
 }
 
+class _ComponentPredictions extends StatelessWidget {
+
+  static final _dateResolver = DateFormat('EEEE', 'it');
+
+  const _ComponentPredictions({
+    required this.campusVodafoneData,
+    required this.neighborhoodVodafoneData,
+  });
+
+  final VodafoneDailyList campusVodafoneData;
+  final VodafoneDailyList neighborhoodVodafoneData;
+
+  @override
+  Widget build(BuildContext context) {
+    // Cache visitors and capture ratio
+    final campusVodafoneVisitors = campusVodafoneData.visitors;
+    final neighborhoodVodafoneVisitors = neighborhoodVodafoneData.visitors;
+    final captureRatio = campusVodafoneVisitors / neighborhoodVodafoneVisitors * 100;
+    // Generate collapsed data
+    final campusCollapsedByAge = campusVodafoneData.collapseFromKPI(KPI.age);
+    final campusCollapsedByNationality = campusVodafoneData.collapseFromKPI(KPI.nationality);
+    final campusCollapsedByGender = campusVodafoneData.collapseFromKPI(KPI.gender);
+    // Generate final data
+    final ageAverage = _generateAgeAverage(campusCollapsedByAge);
+    final foreignersPercentage = _generateForeignersPercentage(campusCollapsedByNationality);
+    // Build UI
+    return Column(
+      children: [
+        ListTile(
+          title: Text('PREVISIONI PER OGGI', style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+          subtitle: Text('Rispetto $_dateString precedenti'),
+        ),
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text('Visitatori attesi'),
+          trailing: Text('${(campusVodafoneData.visitors / campusVodafoneData.length).round()}'),
+        ),
+        ListTile(
+          leading: Icon(Icons.label),
+          title: Text('Età media'),
+          trailing: Text('${ageAverage.round()} anni'),
+        ),
+        ListTile(
+          leading: Icon(Icons.home),
+          title: Text('Tasso di cattura'),
+          trailing: Text('${captureRatio.toStringAsFixed(2)}%'),
+        ),
+        ListTile(
+          leading: Icon(Icons.person),
+          title: Text('Percentuale di stranieri'),
+          trailing: Text('${foreignersPercentage.toStringAsFixed(2)}%'),
+        ),
+        SizedBox(
+          width: double.infinity, height: MediaQuery.of(context).size.height / 2.5,
+          child: SfCircularChart(
+            legend: Legend(
+              isVisible: true,
+              position: LegendPosition.right,
+            ),
+            series: [
+              PieSeries<VodafoneCluster, String>(
+                dataSource: campusCollapsedByGender,
+                xValueMapper: (datum, index) => '${datum.gender}',
+                yValueMapper: (datum, index) => datum.visitors,
+                pointColorMapper: (datum, index) => datum.gender.color,
+                animationDuration: 0,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  String get _dateString {
+    final now = DateTime.now();
+    if (now.weekday == 7) {
+      return 'alle ${_dateResolver.format(now)}';
+    } else {
+      return 'ai ${_dateResolver.format(now)}';
+    }
+  }
+
+  int _generateAgeAverage(VodafoneDaily collapsedCampus) {
+    double age = 0;
+    for (var cluster in collapsedCampus) {
+      age += cluster.visitors * cluster.age.median;
+    }
+    age /= collapsedCampus.visitors;
+    return age.round();
+  }
+
+  double _generateForeignersPercentage(VodafoneDaily campusNationality) {
+    return campusNationality.singleWhere(
+      (element) => element.nationality == Nationality.foreigner,
+      orElse: () => VodafoneCluster.empty(),
+    ).visitors / campusNationality.visitors * 100;
+  }
+
+}
